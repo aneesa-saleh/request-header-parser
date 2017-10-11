8a69ab7fd846910b9306d2a9c752e0ffee30e97e
System <none>
2017-10-11 16:51:27 +0000

    updated to return language and ip address

--- server.js
+++ server.js
@@ -2,20 +2,43 @@
 
 // init project
 var express = require('express');
+var os = require('os');
 var app = express();
+var device = require('express-device');
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+app.use(device.capture());
 
-// http://expressjs.com/en/starter/static-files.html
+// set dir to serve static files
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// handle home page
 app.get("/", function (request, response) {
   response.status(200);
   response.sendFile(__dirname + '/views/index.html');
 });
 
+// handle header parser request
+app.get("/api/whoami", function (request, response) {
+  response.status(200);
+  var ip = request.header('x-forwarded-for') || request.connection.remoteAddress;
+  ip = ip.split(',')[0];                              //get first value only
+  var language = request.headers["accept-language"]
+                  .split(',')[0];                     //get first value only
+
+      var os_string = os.platform();
+      var header_info = {
+        "IP address": ip,
+        "Language": language,
+        "Operating System": os_string
+      };
+
+  response.send(request.device.type.toUpperCase());
+  //response.json(header_info);
+  //});
+  
+});
+
+//handle 404 (page not found)
 app.get('*', function(request, response){
   response.status(404);
   response.sendFile(__dirname + '/views/404.html');
@@ -23,5 +46,5 @@ app.get('*', function(request, response){
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  //console.log('Your app is listening on port ' + listener.address().port);
 });
